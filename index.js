695ddf6df4f62437bdf866d950404b4b5c585c07
Ian Pring <ian.pring@btinternet.com>
2022-05-10 15:59:30 +0100

    created procfile for heroku

--- index.js
+++ index.js
@@ -2,16 +2,54 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+// API Key pk_8fd4d93086094b8c984c33dd2f5c96e2
+// Create call_api function
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_8fd4d93086094b8c984c33dd2f5c96e2', { json: true}, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode === 200){
+       // {console.log(body);
+        finishedAPI(body);
+        };
+    });  
+};
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-// Set handlebar routes
+// Set handlebar index GET routes
 app.get('/', function (req, res) {
-    res.render('home');
+    call_api(function(doneAPI) {
+            res.render('home', {
+            stock: doneAPI
+        }); 
+    }, "tsla");
+    
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+            //posted_stuff = req.body.stock_ticker;
+            res.render('home', {
+            stock: doneAPI,
+        }); 
+    }, req.body.stock_ticker);
+    
+});
+
+// Create About page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set Static Folder
